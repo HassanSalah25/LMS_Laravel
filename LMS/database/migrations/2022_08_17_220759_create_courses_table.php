8fb590d52b78205942f78b7bfab2c6b884734ab1
Hassan <salemhassan175@gmail.com>
2022-08-18 00:21:22 +0200

    courses and certificate models update

---
+++ LMS/database/migrations/2022_08_17_220759_create_courses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('description');
+            $table->string('cover_image');
+            $table->string('hours');
+            $table->string('certificate_image');
+            $table->string('position');
+            $table->string('is_active');
+            $table->string('created_by');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
