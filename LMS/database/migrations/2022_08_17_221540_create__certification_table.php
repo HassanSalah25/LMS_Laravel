20c3fc93517e64c2650e9d3e2b45e418157787ca
Hassan <salemhassan175@gmail.com>
2022-08-19 03:32:20 +0200

    add new tables (Feedback,Category,Lesson,module)

---
+++ LMS/database/migrations/2022_08_17_221540_create__certification_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCertificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_certification', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("course_id");
+            $table->string("type");
+            $table->string("certificate");
+            $table->string("created_by");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_certification');
+    }
+}
