c3e43e3351cf7b870d3e60705068fec82d8d5e85
Hassan <salemhassan175@gmail.com>
2022-08-20 04:28:38 +0200

    update admin view

---
+++ LMS/resources/views/admin.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+@section('content')
+<body class="vsc-initialized">
+    <div class="content">
+    <div class="container">
+    <h2 class="mb-5">Table #2</h2>
+    <div class="table-responsive">
+    <table class="table custom-table">
+    <thead>
+    <tr>
+    <th scope="col">Order</th>
+    <th scope="col">Name</th>
+    <th scope="col">Occupation</th>
+    <th scope="col">Contact</th>
+    <th scope="col">Education</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr scope="row">
+    <td>
+    1392
+    </td>
+    <td>James Yates</td>
+    <td>
+    Web Designer
+    <small class="d-block">Far far away, behind the word mountains</small>
+    </td>
+    <td>+63 983 0962 971</td>
+    <td>NY University</td>
+    </tr>
+    <tr>
+    <td>4616</td>
+    <td>Matthew Wasil</td>
+    <td>
+    Graphic Designer
+    <small class="d-block">Far far away, behind the word mountains</small>
+    </td>
+    <td>+02 020 3994 929</td>
+    <td>London College</td>
+    </tr>
+    <tr>
+    <td>9841</td>
+    <td>Sampson Murphy</td>
+    <td>
+    Mobile Dev
+    <small class="d-block">Far far away, behind the word mountains</small>
+    </td>
+    <td>+01 352 1125 0192</td>
+    <td>Senior High</td>
+    </tr>
+    <tr>
+    <td>9548</td>
+    <td>Gaspar Semenov</td>
+    <td>
+    Illustrator
+    <small class="d-block">Far far away, behind the word mountains</small>
+    </td>
+    <td>+92 020 3994 929</td>
+    <td>College</td>
+    </tr>
+    </tbody>
+    </table>
+    </div>
+    </div>
+    </div>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js+bootstrap.min.js+main.js.pagespeed.jc.U3dPnPLyQo.js"></script><script>eval(mod_pagespeed_fby5qHOZNL);</script>
+    <script>eval(mod_pagespeed_4RNNd4_oUn);</script>
+    <script>eval(mod_pagespeed_7IiBxSCMrY);</script>
+    <script defer="" src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon="{&quot;rayId&quot;:&quot;73d7aa947d2be914&quot;,&quot;token&quot;:&quot;cd0b4b3a733644fc843ef0b185f98241&quot;,&quot;version&quot;:&quot;2022.8.0&quot;,&quot;si&quot;:100}" crossorigin="anonymous"></script>
+    
+    </body>
+    @endsection
--- LMS/routes/web.php
+++ LMS/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admins', function () {
+    return view('admin');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
