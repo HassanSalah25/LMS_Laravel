a8ff2d8bbce2953f385b83b291a12ae5ebc2fbf2
hassangomaa <animehassan05@gmail.com>
2022-08-20 20:50:20 +0200

    stoop login1

--- LMS/routes/web.php
+++ LMS/routes/web.php
@@ -18,33 +18,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
-Route::get('/admins', function () {
-    return view('admin', [
-        'admins' => Admin::Paginate(5)
-    ]);
-});
+ 
 Route::get('/admin', function () {//NewDesign 
     return view('admin1', [
         'admins' => Admin::Paginate(5)
     ]);
 });
-=======
-Route::get('/dashboard', function () {
-   // return view('Maindashboard/index');
-    return \File::get(public_path() . '/SPAindex.html');
->>>>>>> 95ff3888abbfe766ff73b1a4b92c5709f004a275
 
+ 
 // Route::get('/dashboard', function () {
 //    // return view('Maindashboard/index');
 //     return \File::get(public_path() . '/SPAindex.html');
 
-<<<<<<< HEAD
-// })->middleware(['auth'])->name('dashboard');
+ // })->middleware(['auth'])->name('dashboard');
 
-// require __DIR__.'/auth.php';
-=======
-
+  
 // admin CRUD
 Route::get('/admins', [AdminController::class, 'index'])->name('Admins');
 Route::get('/admins/create', [AdminController::class, 'create'])->name('Admins.create');
@@ -54,4 +42,4 @@ Route::post('/admins/delete/{id}', [AdminController::class, 'destroy'])->name('A
 Route::post('/admins/create', [AdminController::class, 'store'])->name('Admins.store');
 
 require __DIR__.'/auth.php';
->>>>>>> 95ff3888abbfe766ff73b1a4b92c5709f004a275
+ 
